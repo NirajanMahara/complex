341c1beb74f50df122871b4ddc9744d1381091c9
Nirajan Mahara <N.Mahara@wlv.ac.uk>
2022-01-22 14:49:17 +0545

    MongoDB connection setup to database

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import connectDB from './config/db.js';
 import data from './data.js';
 
 const app = express();
+
+connectDB();
 
 app.get('/api/products/:id', (req, res) => {
   const product = data.products.find((x) => x._id === req.params.id);
