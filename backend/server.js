95c5a699df1d7cd61ca08952eb4abae0417b3670
Nirajan Mahara <N.Mahara@wlv.ac.uk>
2022-02-09 21:57:12 +0545

    build(admin products): upload product image

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
 import connectDB from './config/db.js';
 import productRouter from './routers/productRouter.js';
 import userRouter from './routers/userRouter.js';
 import orderRouter from './routers/orderRouter.js';
+import uploadRouter from './routers/uploadRouter.js';
 
 dotenv.config();
 
@@ -14,6 +16,8 @@ app.use(express.urlencoded({ extended: true }));
 
 connectDB();
 
+app.use('/api/uploads', uploadRouter);
+
 app.use('/api/users', userRouter);
 
 app.use('/api/products', productRouter);
@@ -23,6 +27,10 @@ app.use('/api/orders', orderRouter);
 app.get('/api/config/paypal', (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
 });
+
+const __dirname = path.resolve();
+
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 app.get('/', (req, res) => {
   res.send('Server is ready');
