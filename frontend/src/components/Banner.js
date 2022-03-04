19b04404f6851596594eabb9dda3257e5bbe066a
Nirajan Mahara <N.Mahara@wlv.ac.uk>
2022-03-04 21:10:15 +0545

    refactor(fix): style & banner

--- frontend/src/components/Banner.js
+++ frontend/src/components/Banner.js
@@ -3,8 +3,9 @@ import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 function Banner() {
   return (
-    <div className='relative'>
-      <div className='absolute w-full h-32 bg-gradient-to-t from-gray-100 to-transparent bottom-0 z-20' />
+    <div>
+      {/* <div className='relative'> */}
+      {/* <div className='absolute w-full h-32 bg-gradient-to-t from-gray-100 to-transparent bottom-0 z-20' /> */}
 
       <Carousel
         autoPlay
@@ -38,6 +39,7 @@ function Banner() {
           />
         </div>
       </Carousel>
+      {/* </div> */}
     </div>
   );
 }
