c9d5bcd9348a9b823b3b050493f5bd649963dda0
Nirajan Mahara <N.Mahara@wlv.ac.uk>
2022-03-07 23:22:17 +0545

    refactor(build): `react-responsive-carousel` banner in `HomeScreen`

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -4,7 +4,8 @@ import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
 import { useDispatch, useSelector } from 'react-redux';
 import { listProducts } from '../actions/productActions';
-import Banner from '../components/Banner';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 export default function HomeScreen() {
   const dispatch = useDispatch();
@@ -18,7 +19,38 @@ export default function HomeScreen() {
   return (
     <div className='home'>
       <div className='home__banner'>
-        <Banner />
+        <Carousel
+          autoPlay
+          infiniteLoop
+          showStatus={false}
+          showIndicators={false}
+          showThumbs={false}
+          interval={5000}
+        >
+          <div>
+            <img
+              loading='lazy'
+              src='https://res.cloudinary.com/nmahara/image/upload/v1645005426/0504-AMZN-GNBC-GatewayHero-1500x600_v5._CB669739807__y1ippl.jpg'
+              alt=''
+            />
+          </div>
+
+          <div>
+            <img
+              loading='lazy'
+              src='https://res.cloudinary.com/nmahara/image/upload/v1645005426/UGRR_S1_GWBleedingHero_ENG_COVIDUPDATE_XSite_1500X600_PV_en-GB._CB669781769__oyldui.jpg'
+              alt=''
+            />
+          </div>
+
+          <div>
+            <img
+              loading='lazy'
+              src='https://res.cloudinary.com/nmahara/image/upload/v1645005426/UK-EN_030821_SpringSitewide_ACQ_GW_Hero_D_1500x600_CV69._CB656397523__k6qxxf.jpg'
+              alt=''
+            />
+          </div>
+        </Carousel>
       </div>
       {loading ? (
         <LoadingBox></LoadingBox>
